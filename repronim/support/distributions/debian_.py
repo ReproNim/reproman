e70eb1299c145b17131645e28fd6f14aa2b24d09
Yaroslav Halchenko <debian@onerussian.com>
2016-09-22 18:10:21 -0400

    RF: just for demo used dict comprehension in DebianReleaseSpec and exteded a doc with example

--- repronim/support/distributions/debian_.py
+++ repronim/support/distributions/debian_.py
@@ -56,7 +56,43 @@ def get_spec_from_release_file(release_filename):
     release = deb822.Release(codecs.open(release_filename, 'r', 'utf-8'))
     # TODO: redo with conversions of components and architectures in into lists
     # and date in machine-readable presentation
-    return DebianReleaseSpec(*[
-            release.get(a.name.title(), None)
-            for a in attr.fields(DebianReleaseSpec)]
-    )
+    return DebianReleaseSpec(**{
+            a.name: release.get(a.name.title(), None)
+            for a in attr.fields(DebianReleaseSpec)
+    })
+
+
+def get_used_release_specs(package, installed_version=None):
+    """Given a name for an installed package, return a set of specs describing
+    the release this file could be obtained from
+
+    e.g. for
+
+        $> apt-cache policy afni python-nibabel
+        afni:
+          Installed: 16.2.07~dfsg.1-2~nd90+1
+          Candidate: 16.2.07~dfsg.1-2~nd90+1
+          Version table:
+         *** 16.2.07~dfsg.1-2~nd90+1 500
+                500 http://neuro.debian.net/debian stretch/contrib amd64 Packages
+                100 /var/lib/dpkg/status
+        python-nibabel:
+          Installed: 2.1.0-1
+          Candidate: 2.1.0-1
+          Version table:
+         *** 2.1.0-1 900
+                900 http://http.debian.net/debian stretch/main amd64 Packages
+                900 http://http.debian.net/debian stretch/main i386 Packages
+                600 http://http.debian.net/debian sid/main amd64 Packages
+                600 http://http.debian.net/debian sid/main i386 Packages
+                100 /var/lib/dpkg/status
+             2.1.0-1~nd90+1 500
+                500 http://neuro.debian.net/debian stretch/main amd64 Packages
+                500 http://neuro.debian.net/debian stretch/main i386 Packages
+
+    should provide paths to release files
+
+        /var/lib/apt/lists/http.debian.net_debian_dists_stretch_InRelease
+        /var/lib/apt/lists/neuro.debian.net_debian_dists_stretch_InRelease
+
+    """
