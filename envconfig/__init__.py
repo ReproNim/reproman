9987f797777a4995bdcb5ef34016380dda5e7365
Robert Buccigrossi <butch@tcg.com>
2016-11-29 21:08:18 -0500

    Began the concept of an environment configuration registry that will be used to handle, process, and create environment configuration items of a specific name

--- envconfig/__init__.py
+++ envconfig/__init__.py
@@ -6,7 +6,7 @@
 #   copyright and license terms.
 #
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-"""Environment Configuration Classes
+"""ReproNim Experiment Environment Configuration
 
 """
 
