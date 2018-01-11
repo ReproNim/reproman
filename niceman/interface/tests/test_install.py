1dfee7ae09f25260483264374a8346e5cbf4ac11
Matt Travers <matt.travers@tcg.com>
2018-01-11 16:54:33 -0500

    BF: fixed issues in test_interface

--- niceman/interface/tests/test_install.py
+++ niceman/interface/tests/test_install.py
@@ -65,27 +65,22 @@ def test_install_interface(demo1_spec, niceman_cfg_path):
         calls = [
             call(base_url='tcp://127.0.0.1:2375'),
             call().exec_create(cmd=['[', '-e', '/etc/apt/sources.list.d/niceman.sources.list', ']'], container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
-            call().exec_create(cmd='sh -c \'echo "# Niceman repo sources" > /etc/apt/sources.list.d/niceman.sources.list\'', container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
+            call().exec_create(cmd="grep -q 'deb http://snapshot.debian.org/archive/debian/20170531T084046Z/ sid main contrib non-free' /etc/apt/sources.list.d/niceman.sources.list", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
+            call().exec_create(cmd="grep -q 'deb http://snapshot.debian.org/archive/debian/20171208T032012Z/ sid main contrib non-free' /etc/apt/sources.list.d/niceman.sources.list", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
             call().exec_create(cmd="grep -q 'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free' /etc/apt/sources.list.d/niceman.sources.list", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
-            call().exec_create(cmd="sh -c 'echo deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free >> /etc/apt/sources.list.d/niceman.sources.list'", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
+            call().exec_create(cmd="grep -q 'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free' /etc/apt/sources.list.d/niceman.sources.list", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
             call().exec_create(cmd=['apt-key', 'adv', '--recv-keys', '--keyserver', 'hkp://pool.sks-keyservers.net:80', '0xA5D32F012649A5A9'], container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
             call().exec_create(cmd="grep -q 'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free' /etc/apt/sources.list.d/niceman.sources.list", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
-            call().exec_create(cmd="sh -c 'echo deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free >> /etc/apt/sources.list.d/niceman.sources.list'", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
+            call().exec_create(cmd="grep -q 'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free' /etc/apt/sources.list.d/niceman.sources.list", container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
             call().exec_create(cmd=['apt-key', 'adv', '--recv-keys', '--keyserver', 'hkp://pool.sks-keyservers.net:80', '0xA5D32F012649A5A9'], container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
             call().exec_create(cmd=['apt-get', '-o', 'Acquire::Check-Valid-Until=false', 'update'], container={'Id': '326b0fdfbf838', 'Names': ['/my-resource'], 'State': 'running'}),
         ]
-
         client.assert_has_calls(calls, any_order=True)
 
         assert_in('Adding Debian update to environment command list.', log.lines)
         assert_in("Running command ['[', '-e', '/etc/apt/sources.list.d/niceman.sources.list', ']']", log.lines)
-        assert_in('Running command \'sh -c \\\'echo "# Niceman repo sources" > /etc/apt/sources.list.d/niceman.sources.list\\\'\'', log.lines)
+        assert_in('Running command "grep -q \'deb http://snapshot.debian.org/archive/debian/20170531T084046Z/ sid main contrib non-free\' /etc/apt/sources.list.d/niceman.sources.list"', log.lines)
+        assert_in('Running command "grep -q \'deb http://snapshot.debian.org/archive/debian/20171208T032012Z/ sid main contrib non-free\' /etc/apt/sources.list.d/niceman.sources.list"', log.lines)
         assert_in('Running command "grep -q \'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free\' /etc/apt/sources.list.d/niceman.sources.list"', log.lines)
-        assert_in("Adding line 'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free' to /etc/apt/sources.list.d/niceman.sources.list", log.lines)
-        assert_in('Running command "sh -c \'echo deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free >> /etc/apt/sources.list.d/niceman.sources.list\'"', log.lines)
-        assert_in("Running command ['apt-key', 'adv', '--recv-keys', '--keyserver', 'hkp://pool.sks-keyservers.net:80', '0xA5D32F012649A5A9']", log.lines)
-        assert_in('Running command "grep -q \'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free\' /etc/apt/sources.list.d/niceman.sources.list"', log.lines)
-        assert_in("Adding line 'deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free' to /etc/apt/sources.list.d/niceman.sources.list", log.lines)
-        assert_in('Running command "sh -c \'echo deb http://snapshot-neuro.debian.net:5002/archive/neurodebian/20171208T032012Z/ xenial main contrib non-free >> /etc/apt/sources.list.d/niceman.sources.list\'"', log.lines)
         assert_in("Running command ['apt-key', 'adv', '--recv-keys', '--keyserver', 'hkp://pool.sks-keyservers.net:80', '0xA5D32F012649A5A9']", log.lines)
         assert_in("Running command ['apt-get', '-o', 'Acquire::Check-Valid-Until=false', 'update']", log.lines)
