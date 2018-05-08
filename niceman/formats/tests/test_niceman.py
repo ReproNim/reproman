c99e39ed056d05fd7bb15fda4bb2680e222dd0f9
Kyle Meyer <kyle@kyleam.com>
2018-05-07 22:24:22 -0400

    TST: Add dump reload test for NicemanProvenance
    This would catch the error fixed by 3b8a9884 (i.e. objects at the
    level of CondaPackage's or deeper not being converted from dicts to
    their respective types).  But, in order to show it fails, you need to
    apply the niceman/distributions/conda.py change from 3b8a9884 so that
    all of CondaPackage's arguments aren't required.

--- niceman/formats/tests/test_niceman.py
+++ niceman/formats/tests/test_niceman.py
@@ -11,6 +11,17 @@ from __future__ import absolute_import
 
 import io
 
+from niceman.distributions.base import EnvironmentSpec
+from niceman.distributions.conda import CondaChannel
+from niceman.distributions.conda import CondaDistribution
+from niceman.distributions.conda import CondaEnvironment
+from niceman.distributions.conda import CondaPackage
+from niceman.distributions.debian import APTSource
+from niceman.distributions.debian import DEBPackage
+from niceman.distributions.debian import DebianDistribution
+from niceman.distributions.venv import VenvDistribution
+from niceman.distributions.venv import VenvEnvironment
+from niceman.distributions.venv import VenvPackage
 from niceman.formats.niceman import NicemanProvenance
 
 from .constants import NICEMAN_SPEC1_YML_FILENAME
@@ -32,3 +43,91 @@ def test_write():
     # and we could do the full round trip while retaining the same "value"
     assert env == env_reparsed
     print(out)
+
+
+def test_spec_round_trip():
+    # FIXME: This should also test GitDistribution's, but NicemanProvenance
+    # can't currently load those (gh-222).
+
+    spec = EnvironmentSpec(
+        distributions=[
+            DebianDistribution(
+                name="debian",
+                apt_sources=[
+                    APTSource(
+                        name="apt_Debian_stable_main_0",
+                        component="main",
+                        archive="stable",
+                        architecture="amd64",
+                        codename="stretch",
+                        origin="Debian",
+                        label="Debian",
+                        site="ftp.us.debian.org",
+                        archive_uri="http://ftp.us.debian.org/debian",
+                        date="2018-03-10 10:21:19+00:00")],
+                packages=[
+                    DEBPackage(name="debpackage"),
+                    DEBPackage(
+                        name="libc-bin",
+                        upstream_name=None,
+                        version="2.24-11+deb9u3",
+                        architecture="amd64",
+                        source_name="glibc",
+                        source_version=None,
+                        size="779468",
+                        md5="3b9aaa83b5253895b8e13509659662e4",
+                        sha1=None,
+                        sha256="aaa",
+                        versions={"2.24-11+deb9u1": ["apt_Debian_stable_foo"],
+                                  "2.24-11+deb9u3": ["apt_Debian_stable_bar",
+                                                     "apt__now__0"]},
+                        install_date="2018-03-12 10:55:13+00:00",
+                        files=["/usr/bin/zdump"])],
+                version="9.4"),
+            CondaDistribution(
+                name="conda",
+                path="/path/to/miniconda3",
+                conda_version="4.4.10",
+                python_version="3.6.3.final.0",
+                platform="linux-64",
+                environments=[
+                    CondaEnvironment(
+                        name="root",
+                        path="/path/to/miniconda3",
+                        packages=[
+                            CondaPackage(name="condapkg"),
+                            CondaPackage(
+                                version="36.5.0",
+                                build="py36he42e2e1_0",
+                                name="setuptools",
+                                md5="cb1383539629db998105faf7e91e2bc7",
+                                url="https://somewhere")],
+                        channels=[
+                            CondaChannel(
+                                name="defaults",
+                                url="https://somewhere")]),
+                    CondaEnvironment(
+                        name="other",
+                        path="/path/to/miniconda3",
+                        packages=[
+                            CondaPackage(name="condapkg2")])]),
+            VenvDistribution(
+                name="venv0",
+                path="/usr/bin/virtualenv",
+                venv_version="15.1.0",
+                environments=[
+                    VenvEnvironment(
+                        path="venv-niceman",
+                        python_version="3.5.3",
+                        packages=[
+                            VenvPackage(
+                                version="3.12",
+                                name="PyYAML",
+                                location="/path/to/venv/site-packages",
+                                local=True)])]),
+            VenvDistribution(name="venv1")])
+
+    output = io.StringIO()
+    NicemanProvenance.write(output, spec)
+    loaded = NicemanProvenance(output.getvalue()).get_environment()
+    assert spec == loaded
