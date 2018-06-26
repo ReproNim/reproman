173b47868eba07179fa4491d5590fa642f647f07
Kyle Meyer <kyle@kyleam.com>
2018-06-25 22:40:47 -0400

    TST: Correct an emacs variable name
    Otherwise Emacs users get prompted about setting a potentially unsafe
    local variable.  (In general, setting file local variables at the top
    of each file isn't a good approach; if we want to suggest some Emacs
    settings for this project, we should set them once in .dir-locals.el.)

--- niceman/ui/tests/test_base.py
+++ niceman/ui/tests/test_base.py
@@ -1,4 +1,4 @@
-# emacs: -*- mode: python; py-indent-offset: 4; tab-wstrth: 4; indent-tabs-mode: nil -*-
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
 # ex: set sts=4 ts=4 sw=4 noet:
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
