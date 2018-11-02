ReproNim/reproman

commit 309bdbbe805bc1f145164dcfd8f44b03f9223bcf

RF: Absorb more resource handling into ResourceManager
Incorporate parts of @yarikoptic's gh-98 (yarikoptic/rf-manager),
focusing on only the changes that concerned the ResourceManager.  Like
that PR, reduce the process of obtaining a resource to a single method
call, handling the inventory behind the scenes, and break up
get_resource_info into more methods.

Also, like that PR, rework resource name/ID handling.  Try to infer
whether the resource references a name or ID, and, for cases where a
reference is ambiguous, expose a flag that enables specifying whether
a reference is a name or ID.  Each resource reference is consistently
called a "resref" in the code to avoid confusion with a resource
instance.

The interface tests have become a little more cumbersome because we
now have to mock an instance rather than the class.  For clarity and
maintainability, it's probably worth exploring whether we can make a
fixture or some other wrapper that simplifies the process.

niceman/interface/retrace.py
@@ -16,6 +16,8 @@ import sys
 import time
 
 from niceman.resource.session import get_local_session
+from .common_opts import resref_opt
+from .common_opts import resref_type_opt
 from .base import Interface
 from ..support.constraints import EnsureNone
 from ..support.constraints import EnsureStr
@@ -23,6 +25,7 @@ from ..support.exceptions import InsufficientArgumentsError
 from ..support.param import Parameter
 from ..utils import assure_list
 from ..utils import to_unicode
+from ..resource import manager
 
 __docformat__ = 'restructuredtext'
 
@@ -61,17 +64,15 @@ class Retrace(Interface):
             metavar='output_file',
             constraints=EnsureStr() | EnsureNone(),
         ),
-        # TODO: make a common arg
-        resource=Parameter(
-            args=("--resource",),
-            doc="TODO"
-        )
+        resref=resref_opt,
+        resref_type=resref_type_opt,
     )
 
     # TODO: add a session/resource so we could trace within
     # arbitrary sessions
     @staticmethod
-    def __call__(path=None, spec=None, output_file=None, resource=None):
+    def __call__(path=None, spec=None, output_file=None,
+                 resref=None, resref_type="auto"):
         # heavy import -- should be delayed until actually used
 
         if not (spec or path):
@@ -92,9 +93,9 @@ class Retrace(Interface):
         # The tracers assume normalized paths.
         paths = list(map(normpath, paths))
 
-        if resource:
-            # TODO: if not a session already, request a session from the resource
-            session = resource
+        if resref:
+            resource = manager.get_resource(resref, resref_type)
+            session = resource.get_session()
         else:
             session = get_local_session()
 

niceman/interface/base.py
@@ -274,27 +274,6 @@ def backend_help(resource_type=None):
     return help_message + ", ".join(help_args)
 
 
-def backend_set_config(params, env_resource, config):
-    """Set backend parameters in resource instance and config.
-
-    Parameters
-    ----------
-    params : list of str
-        A list of backend parameters, where key value pairs are separated by
-        '='.
-    env_resource : Resource object
-    config : dict
-        Configuration parameters for the resource.
-    """
-    for backend_arg in params:
-        key, value = backend_arg.split("=")
-        if hasattr(env_resource, key):
-            config[key] = value
-            setattr(env_resource, key, value)
-        else:
-            raise NotImplementedError("Bad --backend parameter '{}'".format(key))
-
-
 class Interface(object):
     """Base class for interface implementations"""
 

niceman/interface/common_opts.py
@@ -13,19 +13,39 @@
 __docformat__ = 'restructuredtext'
 
 from niceman.support.param import Parameter
+from niceman.support.constraints import EnsureChoice
 from niceman.support.constraints import EnsureInt, EnsureNone, EnsureStr
+
 
 trace_opt = Parameter(
     args=("--trace",),
     action="store_true",
     doc="""if set, trace execution within the environment""")
 
-resource_name_opt = Parameter(
-    args=("-n", "--name",),
-    doc="Name of the environment container",
+
+#
+# Resource specifications
+#
+
+resref_arg = Parameter(
+    args=("resref",),
+    metavar="RESOURCE",
+    doc="""Name or ID of the resource to operate on. To see available resources, run
+    'niceman ls'""",
     constraints=EnsureStr() | EnsureNone())
 
-resource_id_opt = Parameter(
-    args=("-id", "--resource-id",),
-    doc="ID of the environment container",
+resref_opt = Parameter(
+    args=("-r", "--resource",),
+    dest="resref",
+    metavar="RESOURCE",
+    doc="""Name or ID of the resource to operate on. To see available resources, run
+    'niceman ls'""",
     constraints=EnsureStr() | EnsureNone())
+
+resref_type_opt = Parameter(
+    args=("--resref-type",),
+    metavar="TYPE",
+    doc="""A resource can be referenced by its name or ID.  In the unlikely
+    case that a name collides with an ID, explicitly specify 'name' or 'id' to
+    disambiguate.""",
+    constraints=EnsureChoice("auto", "name", "id"))

niceman/interface/create.py
@@ -11,13 +11,12 @@
 
 __docformat__ = 'restructuredtext'
 
-from .base import Interface, backend_help, backend_set_config
+from .base import Interface, backend_help
 import niceman.interface.base # Needed for test patching
 from ..support.param import Parameter
 from ..support.constraints import EnsureStr
 from ..support.exceptions import ResourceError
-from ..resource import ResourceManager
-from ..resource import Resource
+from ..resource import manager
 from ..dochelpers import exc_str
 
 from logging import getLogger
@@ -103,30 +102,17 @@ class Create(Interface):
                 error_message="Missing resource name"
             )
 
+        if not resource_type:
+            resource_type = ui.question(
+                "Enter a resource type",
+                default="docker-container"
+            )
         # if only_env:
         #     raise NotImplementedError
 
         # TODO: Add ability to clone a resource.
 
-        # Get configuration and environment inventory
-        config, inventory = ResourceManager.get_resource_info(
-            name, type_=resource_type)
-
-        # Create resource environment
-        env_resource = ResourceManager.factory(config)
-
-        # Set resource properties to any backend specific command line arguments.
-        if backend:
-            backend_set_config(backend, env_resource, config)
-
-        env_resource.connect()
-        resource_attrs = env_resource.create()
-
-        # Save the updated configuration for this resource.
-        config.update(resource_attrs)
-        inventory[name] = config
-        ResourceManager.set_inventory(inventory)
-
+        manager.create(name, resource_type, backend)
         lgr.info("Created the environment %s", name)
 
         # TODO: at the end install packages using install and created env

niceman/interface/delete.py
@@ -15,11 +15,11 @@ import re
 
 from .base import Interface
 import niceman.interface.base # Needed for test patching
-from .common_opts import resource_id_opt
-from .common_opts import resource_name_opt
+from .common_opts import resref_arg
+from .common_opts import resref_type_opt
 from ..support.param import Parameter
 from ..support.constraints import EnsureStr
-from ..resource import ResourceManager
+from ..resource import manager
 
 from logging import getLogger
 lgr = getLogger('niceman.api.delete')
@@ -31,19 +31,19 @@ class Delete(Interface):
     Examples
     --------
 
-      $ niceman delete --name=my-resource
+      $ niceman delete my-resource
 
     """
 
     _params_ = dict(
-        name=resource_name_opt,
+        resref=resref_arg,
         # XXX reenable when we support working with multiple instances at once
         # resource_type=Parameter(
         #     args=("-t", "--resource-type"),
         #     doc="""Resource type to work on""",
         #     constraints=EnsureStr(),
         # ),
-        resource_id=resource_id_opt,
+        resref_type=resref_type_opt,
         skip_confirmation=Parameter(
             args=("--skip-confirmation",),
             action="store_true",
@@ -52,42 +52,21 @@ class Delete(Interface):
     )
 
     @staticmethod
-    def __call__(name, resource_id=None, skip_confirmation=False):
+    def __call__(resref, resref_type="auto", skip_confirmation=False):
         from niceman.ui import ui
-        if not name and not resource_id:
-            name = ui.question(
-                "Enter a resource name",
-                error_message="Missing resource name"
-            )
 
-        # Get configuration and environment inventory
-        # TODO: this one would ask for resource type whenever it is not found
-        #       why should we???
-        resource_info, inventory = ResourceManager.get_resource_info(name, resource_id)
-
-        # Delete resource environment
-        env_resource = ResourceManager.factory(resource_info)
-        env_resource.connect()
-
-        if not env_resource.id:
-            raise ValueError("No resource found given the info %s" % str(resource_info))
+        resource = manager.get_resource(resref, resref_type)
 
         if skip_confirmation:
             response = True
         else:
             response = ui.yesno(
                 "Delete the resource '{}'? (ID: {})".format(
-                    env_resource.name, env_resource.id[:20]),
+                    resource.name, resource.id[:20]),
                 default="no"
             )
 
         if response:
-            env_resource.delete()
-
-            # Save the updated configuration for this resource.
-            if name in inventory:
-                del inventory[name]
-
-            ResourceManager.set_inventory(inventory)
-
-            lgr.info("Deleted the environment %s", name)
+            resource.connect()
+            manager.delete(resource)
+            lgr.info("Deleted the environment %s", resource.name)

niceman/interface/exec.py
@@ -22,11 +22,11 @@ from ..support.exceptions import CommandError
 import niceman.interface.base  # Needed for test patching
 from ..support.param import Parameter
 from ..support.constraints import EnsureStr, EnsureNone
-from ..resource import ResourceManager
+from ..resource import manager
 from ..resource.session import Session
 from .common_opts import trace_opt
-from .common_opts import resource_id_opt
-from .common_opts import resource_name_opt
+from .common_opts import resref_opt
+from .common_opts import resref_type_opt
 
 from logging import getLogger
 lgr = getLogger('niceman.api.exec')
@@ -54,14 +54,14 @@ class Exec(Interface):
             nargs="*",
             constraints=EnsureStr(),
         ),
-        name=resource_name_opt,
+        resref=resref_opt,
         # XXX reenable when we support working with multiple instances at once
         # resource_type=Parameter(
         #     args=("-t", "--resource-type"),
         #     doc="""Resource type to work on""",
         #     constraints=EnsureStr(),
         # ),
-        resource_id=resource_id_opt,
+        resref_type=resref_type_opt,
         # TODO: should be moved into generic API
         internal=Parameter(
             args=("--internal",),
@@ -74,27 +74,17 @@ class Exec(Interface):
     )
 
     @staticmethod
-    def __call__(command, args, name=None, resource_id=None,
+    def __call__(command, args, resref=None, resref_type="auto",
                  internal=False, trace=False):
         from niceman.ui import ui
-        if not name and not resource_id:
-            name = ui.question(
-                "Enter a resource name",
-                error_message="Missing resource name"
+        if not resref:
+            resref = ui.question(
+                "Enter a resource name or ID",
+                error_message="Missing resource name or ID"
             )
 
-        # Get configuration and environment inventory
-        # TODO: this one would ask for resource type whenever it is not found
-        #       why should we???
-        resource_info, inventory = ResourceManager.get_resource_info(
-            name, resource_id)
-
-        env_resource = ResourceManager.factory(resource_info)
+        env_resource = manager.get_resource(resref, resref_type)
         env_resource.connect()
-
-        if not env_resource.id:
-            raise ValueError("No resource found given the info %s" % str(resource_info))
-
         session = env_resource.get_session()
 
         remote_env = {}
@@ -215,10 +205,8 @@ class Exec(Interface):
             lgr.info("NICEMAN trace %s", niceman_spec_path)
 
 
-        ResourceManager.set_inventory(inventory)
-
         lgr.info("Executed the %s command in the environment %s", command,
-                 name)
+                 env_resource.name)
 
         if out:
             sys.stdout.write(out)

niceman/interface/install.py
@@ -12,12 +12,12 @@
 __docformat__ = 'restructuredtext'
 
 from .base import Interface
-from .common_opts import resource_id_opt
-from .common_opts import resource_name_opt
+from .common_opts import resref_opt
+from .common_opts import resref_type_opt
 from ..support.param import Parameter
 from ..support.constraints import EnsureStr
 from ..formats import Provenance
-from ..resource import ResourceManager
+from ..resource import manager
 
 from logging import getLogger
 lgr = getLogger('niceman.api.install')
@@ -45,21 +45,21 @@ class Install(Interface):
             # provide options, like --no-exec, etc  per each spec
             # ACTUALLY this type doesn't work for us since it is --spec SPEC SPEC... TODO
         ),
-        name=resource_name_opt,
-        resource_id=resource_id_opt,
+        resref=resref_opt,
+        resref_type=resref_type_opt,
     )
 
     @staticmethod
-    def __call__(spec, name, resource_id):
+    def __call__(spec, resref=None, resref_type="auto"):
 
         from niceman.ui import ui
         if not spec:
             spec = [ui.question("Enter a spec filename", default="spec.yml")]
 
-        if not name and not resource_id:
-            name = ui.question(
-                "Enter a resource name",
-                error_message="Missing resource name"
+        if not resref:
+            resref = ui.question(
+                "Enter a resource name or ID",
+                error_message="Missing resource name or ID"
             )
 
         # Load, while possible merging/augmenting sequentially
@@ -71,10 +71,7 @@ class Install(Interface):
         #  - provenance might contain a 'base' which would instruct which
         #    resource to use
 
-        # Get configuration and environment inventory
-        config, inventory = ResourceManager.get_resource_info(name, resource_id)
-
-        env_resource = ResourceManager.factory(config)
+        env_resource = manager.get_resource(resref, resref_type)
         env_resource.connect()
 
         #  TODOs:

niceman/interface/login.py
@@ -13,10 +13,11 @@ __docformat__ = 'restructuredtext'
 
 import re
 
-from .base import Interface, backend_help, backend_set_config
+from .base import Interface, backend_help
 import niceman.interface.base # Needed for test patching
-from .common_opts import resource_id_opt
-from .common_opts import resource_name_opt
+from niceman.resource import manager
+from .common_opts import resref_arg
+from .common_opts import resref_type_opt
 from ..support.param import Parameter
 from ..support.constraints import EnsureStr
 from ..resource import ResourceManager
@@ -31,52 +32,27 @@ class Login(Interface):
     Examples
     --------
 
-      $ niceman login --name=my-resource
+      $ niceman login my-resource
 
     """
 
     _params_ = dict(
-        name=resource_name_opt,
-        # XXX reenable when we support working with multiple instances at once
-        # resource_type=Parameter(
-        #     args=("-t", "--resource-type"),
-        #     doc="""Resource type to work on""",
-        #     constraints=EnsureStr(),
-        # ),
-        resource_id=resource_id_opt,
         backend=Parameter(
             args=("-b", "--backend"),
             nargs="+",
             doc=backend_help()
         ),
+        resref=resref_arg,
+        resref_type=resref_type_opt,
     )
 
     @staticmethod
-    def __call__(name, backend, resource_id=None):
-        from niceman.ui import ui
-        if not name and not resource_id:
-            name = ui.question(
-                "Enter a resource name",
-                error_message="Missing resource name"
-            )
-
-        # Get configuration and environment inventory
-        # TODO: this one would ask for resource type whenever it is not found
-        #       why should we???
-        # TODO:  config too bad of a name here -- revert back to resource_info?
-        config, inventory = ResourceManager.get_resource_info(name, resource_id)
+    def __call__(resref, resref_type="auto", backend=None):
+        # Get a corresponding known resource
+        env_resource = manager.get_resource(resref, resref_type)
 
         # Connect to resource environment
-        env_resource = ResourceManager.factory(config)
-
-        # Set resource properties to any backend specific command line arguments.
-        if backend:
-            backend_set_config(backend, env_resource, config)
-
         env_resource.connect()
-
-        if not env_resource.id:
-            raise ValueError("No resource found given the info %s" % str(config))
-
         with env_resource.get_session(pty=True):
             pass
+        # env_resource.login()

commit 524238b88d7f14aae98879d648e46dcad5787c37

BF: retrace: Let Python callers pass a session instance
Before 309bdbbe (RF: Absorb more resource handling into
ResourceManager, 2018-07-27), retrace accepted a Session instance as
its `resource` argument.  That was a bit confusing because it was
undocumented and because, despite the parameter name, a resource
object wasn't accepted.  (The previous code was also buggy because a
CLI caller couldn't specify a resource).  But `execute --trace`
expects to be able to pass a session instance to `retrace`, so
309bdbbe introduced a regression by removing that handling.

Passing session instances may be something we want to allow more
generally in the Python API, but for now just add a kludge to
`retrace` so that a Session instance can be passed for `resref`.

Fixes #316.

niceman/interface/execute.py
@@ -205,7 +205,7 @@ class Execute(Interface):
             retrace(
                 spec=op.join(local_trace_dir, "config.yml"),
                 output_file=niceman_spec_path,
-                resource=session
+                resref=session
             )
             lgr.info("NICEMAN trace %s", niceman_spec_path)
 

niceman/interface/retrace.py
@@ -16,6 +16,7 @@ import sys
 import time
 
 from niceman.resource.session import get_local_session
+from niceman.resource.session import Session
 from .common_opts import resref_opt
 from .common_opts import resref_type_opt
 from .base import Interface
@@ -64,7 +65,14 @@ class Retrace(Interface):
             metavar='output_file',
             constraints=EnsureStr() | EnsureNone(),
         ),
-        resref=resref_opt,
+        resref=Parameter(
+            args=("-r", "--resource",),
+            dest="resref",
+            metavar="RESOURCE",
+            doc="""Name or ID of the resource to operate on. To see available
+            resources, run 'niceman ls'.[PY: Note: As a special case, a session
+            instance can be passed as the value for `resref`.  PY]""",
+            constraints=EnsureStr() | EnsureNone()),
         resref_type=resref_type_opt,
     )
 
@@ -93,7 +101,11 @@ class Retrace(Interface):
         # The tracers assume normalized paths.
         paths = list(map(normpath, paths))
 
-        if resref:
+        if isinstance(resref, Session):
+            # TODO: Special case for Python callers.  Is this something we want
+            # to handle more generally at the interface level?
+            session = resref
+        elif resref:
             resource = get_manager().get_resource(resref, resref_type)
             session = resource.get_session()
         else: