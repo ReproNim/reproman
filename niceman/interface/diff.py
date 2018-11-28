bff5c8d2d67f72d660968e7f1c78127611dbcf00
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2018-11-28 21:05:11 +0000

    RF: moved diff output to result_renderer_cmdline()

--- niceman/interface/diff.py
+++ niceman/interface/diff.py
@@ -69,7 +69,7 @@ class Diff(Interface):
         env_1 = NicemanProvenance(prov1).get_environment()
         env_2 = NicemanProvenance(prov2).get_environment()
 
-        status = 0
+        result = {'distributions': []}
 
         # distribution type -> package type string
         supported_distributions = {
@@ -84,14 +84,11 @@ class Diff(Interface):
         all_dist_types = env_1_dist_types.union(env_2_dist_types)
 
         for dist_type in all_dist_types:
-
             if dist_type not in supported_distributions:
-
                 msg = 'diff doesn\'t know how to handle %s' % str(dist_type)
                 raise ValueError(msg)
-
-            pkg_type = supported_distributions[dist_type]
-
+            dist_res = {'pkg_type': supported_distributions[dist_type], 
+                        'pkg_diffs': []}
             dist_1 = env_1.get_distribution(dist_type)
             if dist_1:
                 pkgs_1 = {p._diff_cmp_id: p for p in dist_1.packages}
@@ -102,57 +99,65 @@ class Diff(Interface):
                 pkgs_2 = {p._diff_cmp_id: p for p in dist_2.packages}
             else:
                 pkgs_2 = {}
-
+            dist_res['pkgs_1'] = pkgs_1
+            dist_res['pkgs_2'] = pkgs_2
             pkgs_1_s = set(pkgs_1)
             pkgs_2_s = set(pkgs_2)
-
-            pkgs_only_1 = pkgs_1_s - pkgs_2_s
-            pkgs_only_2 = pkgs_2_s - pkgs_1_s
-
-            if pkgs_only_1 or pkgs_only_2:
-                print(_make_plural(pkg_type) + ':')
-
-            if pkgs_only_1:
-                for cmp_key in sorted(pkgs_only_1):
-                    package = pkgs_1[cmp_key]
-                    print('< %s' % package.diff_identity_string)
-                    status = 3
-            if pkgs_only_2 and pkgs_only_2:
-                print('---')
-            if pkgs_only_2:
-                for cmp_key in sorted(pkgs_only_2):
-                    package = pkgs_2[cmp_key]
-                    print('> %s' % package.diff_identity_string)
-                status = 3
-
+            dist_res['pkgs_only_1'] = pkgs_1_s - pkgs_2_s
+            dist_res['pkgs_only_2'] = pkgs_2_s - pkgs_1_s
             for cmp_key in pkgs_1_s.intersection(pkgs_2_s):
                 package_1 = pkgs_1[cmp_key]
                 package_2 = pkgs_2[cmp_key]
                 if package_1._diff_vals != package_2._diff_vals:
-                    print('%s %s:' % (pkg_type, " ".join(cmp_key)))
-                    print('< %s' % package_1.diff_subidentity_string)
-                    print('---')
-                    print('> %s' % package_2.diff_subidentity_string)
-                    status = 3
+                    dist_res['pkg_diffs'].append((package_1, package_2))
+            result['distributions'].append(dist_res)
 
         files1 = set(env_1.files)
         files2 = set(env_2.files)
+        result['files_1_only'] = files1 - files2
+        result['files_2_only'] = files2 - files1
 
-        files_1_only = files1 - files2
-        files_2_only = files2 - files1
-
-        if files_1_only or files_2_only:
-            print('Files:')
-            for fname in files_1_only:
-                print('< %s' % fname)
-            if files_1_only and files_2_only:
-                print('---')
-            for fname in files_2_only:
-                print('> %s' % fname)
-            status = 3
-
-        return {'status': status}
+        return result
 
     @staticmethod
     def result_renderer_cmdline(result):
-        return result['status']
+
+        status = 0
+
+        for dist_res in result['distributions']:
+
+            if dist_res['pkgs_only_1'] or dist_res['pkgs_only_2']:
+                print(_make_plural(dist_res['pkg_type']) + ':')
+
+            if dist_res['pkgs_only_1']:
+                for cmp_key in sorted(dist_res['pkgs_only_1']):
+                    package = dist_res['pkgs_1'][cmp_key]
+                    print('< %s' % package.diff_identity_string)
+                status = 3
+            if dist_res['pkgs_only_1'] and dist_res['pkgs_only_2']:
+                print('---')
+            if dist_res['pkgs_only_2']:
+                for cmp_key in sorted(dist_res['pkgs_only_2']):
+                    package = dist_res['pkgs_2'][cmp_key]
+                    print('> %s' % package.diff_identity_string)
+                status = 3
+
+            for (package_1, package_2) in dist_res['pkg_diffs']:
+                print('%s %s:' % (dist_res['pkg_type'], 
+                                  " ".join(package_1._diff_cmp_id)))
+                print('< %s' % package_1.diff_subidentity_string)
+                print('---')
+                print('> %s' % package_2.diff_subidentity_string)
+                status = 3
+
+        if result['files_1_only'] or result['files_2_only']:
+            print('Files:')
+            for fname in result['files_1_only']:
+                print('< %s' % fname)
+            if result['files_1_only'] and result['files_2_only']:
+                print('---')
+            for fname in result['files_2_only']:
+                print('> %s' % fname)
+            status = 3
+
+        return status
