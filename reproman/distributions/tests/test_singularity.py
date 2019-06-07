ac89b3b73fa84bbc0d222c798a15b9ce37e50b18
Kyle Meyer <kyle@kyleam.com>
2019-06-07 12:08:11 -0400

    TST: distribution.singularity: Test just one source
    Testing the local image and URL cases from different sources doesn't
    add much value.  Let's simplify the test code by using only one
    source.

--- reproman/distributions/tests/test_singularity.py
+++ reproman/distributions/tests/test_singularity.py
@@ -26,36 +26,23 @@ def test_singularity_trace(tmpdir):
     tmpdir = str(tmpdir)
     # Download and set up singularity image file
     runner = Runner(cwd=tmpdir)
-    runner.run(['singularity', 'pull', '--name', 'img',
-        'shub://vsoch/hello-world@42e1f04ed80217895f8c960bdde6bef4d34fab59'])
-    image_file = op.join(tmpdir, "img")
+    location = 'vsoch/hello-world@42e1f04ed80217895f8c960bdde6bef4d34fab59'
+    runner.run(['singularity', 'pull', '--name', 'img', 'shub://' + location])
 
     # Test tracer class
     tracer = SingularityTracer()
 
-    files = [image_file, 'non-existent-image']
-    dist, remaining_files = next(tracer.identify_distributions(files))
+    for path in [op.join(tmpdir, "img"), "shub:/" + location]:
+        files = [path, 'non-existent-image']
+        dist, remaining_files = next(tracer.identify_distributions(files))
 
-    assert dist.images[0].md5 == 'ed9755a0871f04db3e14971bec56a33f'
-    assert dist.images[0].bootstrap == 'docker'
-    assert dist.images[0].maintainer == 'vanessasaur'
-    assert dist.images[0].deffile == 'Singularity'
-    assert dist.images[0].schema_version == '1.0'
-    assert dist.images[0].build_date == '2017-10-15T12:52:56+00:00'
-    assert dist.images[0].build_size == '333MB'
-    assert dist.images[0].singularity_version == '2.4-feature-squashbuild-secbuild.g780c84d'
-    assert dist.images[0].base_image == 'ubuntu:14.04'
-    assert 'non-existent-image' in remaining_files
-
-    # Run tracer against a Singularity Hub url
-    files = ['shub:/GodloveD/lolcow@a59d8de3121579fe9c95ab8af0297c2e3aefd827']
-    dist, remaining_files = next(tracer.identify_distributions(files))
-    assert dist.images[0].md5 == 'ee4aae1ea378ad7c0299b308c703187a'
-    assert dist.images[0].bootstrap == 'docker'
-    assert dist.images[0].deffile == 'Singularity'
-    assert dist.images[0].schema_version == '1.0'
-    assert dist.images[0].build_date == '2017-10-17T19:23:53+00:00'
-    assert dist.images[0].build_size == '336MB'
-    assert dist.images[0].singularity_version == '2.4-feature-squashbuild-secbuild.g217367c'
-    assert dist.images[0].base_image == 'ubuntu:16.04'
-    assert dist.images[0].url == 'shub://GodloveD/lolcow@a59d8de3121579fe9c95ab8af0297c2e3aefd827'
+        assert dist.images[0].md5 == 'ed9755a0871f04db3e14971bec56a33f'
+        assert dist.images[0].bootstrap == 'docker'
+        assert dist.images[0].maintainer == 'vanessasaur'
+        assert dist.images[0].deffile == 'Singularity'
+        assert dist.images[0].schema_version == '1.0'
+        assert dist.images[0].build_date == '2017-10-15T12:52:56+00:00'
+        assert dist.images[0].build_size == '333MB'
+        assert dist.images[0].singularity_version == '2.4-feature-squashbuild-secbuild.g780c84d'
+        assert dist.images[0].base_image == 'ubuntu:14.04'
+        assert 'non-existent-image' in remaining_files
