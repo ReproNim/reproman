29da28294e228aee909936d5d0d2b9bcc715054e
Kyle Meyer <kyle@kyleam.com>
2019-07-19 18:30:30 -0400

    ENH: parse_kv_list: Improve error message for invalid key=value pair
    `run` and `create` use parse_kv_list() to parse key-value pairs
    provided on the command line.  If a caller doesn't use the expected
    "key=value" form, we fail unhelpfully within parse_kv_list():
    
        dictionary update sequence element #0 has length 1; 2 is required
    
    Say
    
        Expected 'key=value' format but got 'blahbert'
    
    instead.

--- reproman/tests/test_utils.py
+++ reproman/tests/test_utils.py
@@ -572,6 +572,13 @@ def test_merge_dicts():
     assert merge_dicts([{1: 1}, {2: 2}, {1: 3}]) == {1: 3, 2: 2}
 
 
+@pytest.mark.parametrize("value", [["ab"], ["a=b", "c"]])
+def test_parse_kv_list_invalid(value):
+    with pytest.raises(ValueError) as exc:
+        assert parse_kv_list(value)
+    assert "key=value" in str(exc.value)
+
+
 def test_parse_kv_list():
     for value, expected in [(["a=b"], {"a": "b"}),
                             (["a="], {"a": ""}),
