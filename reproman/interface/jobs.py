ReproNim/reproman

commit ab1c577c57f9625009bf11d224a129e168de48d3

RF: jobs: Continue if action raises OrchestratorError
We're bound to hit this at some point.  Log it and continue with the
next job.

reproman/interface/jobs.py
@@ -14,12 +14,14 @@ import operator
 import logging
 import yaml
 
+from reproman.dochelpers import exc_str
 from reproman.interface.base import Interface
 from reproman.support.jobs.local_registry import LocalRegistry
 from reproman.support.jobs.orchestrators import ORCHESTRATORS
 from reproman.resource import get_manager
 from reproman.support.param import Parameter
 from reproman.support.constraints import EnsureChoice
+from reproman.support.exceptions import OrchestratorError
 from reproman.support.exceptions import ResourceNotFoundError
 from reproman.utils import chpwd
 
@@ -212,6 +214,8 @@ class Jobs(Interface):
             for job in jobs:
                 try:
                     fn(job)
+                except OrchestratorError as exc:
+                    lgr.error("job %s failed: %s", job, exc_str(exc))
                 except ResourceNotFoundError as exc:
                     lgr.error("Resource %s (%s) no longer exists",
                               job["resource_id"], job["resource_name"])

reproman/interface/tests/test_run.py
@@ -22,6 +22,7 @@ from reproman.interface.run import _combine_job_specs
 from reproman.utils import chpwd
 from reproman.utils import swallow_logs
 from reproman.utils import swallow_outputs
+from reproman.support.exceptions import OrchestratorError
 from reproman.support.exceptions import ResourceNotFoundError
 from reproman.tests import fixtures
 from reproman.tests.utils import create_tree
@@ -321,3 +322,25 @@ def test_jobs_deleted_resource(context):
             assert "todelete" not in output.out
             # ... but the alive one will.
             assert "myshell" in output.out
+
+
+def test_jobs_orc_error(context):
+    run = context["run_fn"]
+    jobs = context["jobs_fn"]
+    registry = context["registry"]
+
+    run(command=["doesntmatter1"], resref="myshell")
+
+    jobfiles = registry.find_job_files()
+    assert len(jobfiles) == 1
+
+    with swallow_outputs() as output:
+        with swallow_logs(new_level=logging.ERROR) as log:
+            def die_orc(*args, **kwargs):
+                raise OrchestratorError("resurrection failed")
+
+            with patch("reproman.interface.jobs.show_oneline",
+                       side_effect=die_orc):
+                jobs(queries=[], status=True)
+            assert "myshell" not in output.out
+            assert "resurrection failed" in log.out

commit e1f417ea83e7230dd772a27d92d40ca7f5b759a6

BF: jobs: Don't dump job spec when logging orchestrators errors
When we started logging orchestrator errors in ab1c577c5 (RF: jobs:
Continue if action raises OrchestratorError, 2019-03-04), we put the
entire job spec into the log message.  That provides a flood of
information that is hard for the user to parse.  Replace the job spec
with the job ID, which was presumably the intended value in the first
place.

reproman/interface/jobs.py
@@ -216,7 +216,7 @@ class Jobs(Interface):
                 try:
                     fn(job)
                 except OrchestratorError as exc:
-                    lgr.error("job %s failed: %s", job, exc_str(exc))
+                    lgr.error("job %s failed: %s", job["_jobid"], exc_str(exc))
                 except ResourceNotFoundError:
                     lgr.error("Resource %s (%s) no longer exists",
                               job["resource_id"], job["resource_name"])