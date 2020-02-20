64ba4cec4d49e5367bf2d7c48ff2d2b338cafd01
Kyle Meyer <kyle@kyleam.com>
2020-02-20 16:07:03 -0500

    MNT: setup.py: Set python_requires
    This should have been done in gh-374 when we dropped Python 2 support.

--- setup.py
+++ setup.py
@@ -144,6 +144,7 @@ setup(
     description="Neuroimaging Computational Environments Manager",
     long_description=long_description,
     packages=reproman_pkgs,
+    python_requires='>=3.4',
     install_requires=requires['core'],
     extras_require=requires,
     entry_points={
